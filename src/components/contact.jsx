8dd745e42190b6d3fb15b1f80bc10ed90247e19f
Harpreet Sandhu <harpsandhu@Harpreets-MacBook-Air.local>
2021-07-16 22:53:41 -0400

    footer removed

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -162,7 +162,7 @@ export const Contact = (props) => {
           </div>
         </div>
       </div>
-      <div id='footer'>
+      {/* <div id='footer'>
         <div className='container text-center'>
           <p>
             &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
@@ -171,7 +171,7 @@ export const Contact = (props) => {
             </a>
           </p>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
